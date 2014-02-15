5e23c69ac45d4771c9d5f90a32e3fd93b50bddd2
Luke Puchner-Hardman <x.lukk3.x@gmail.com>
2014-02-15 14:14:39 -0600

    player

---
+++ src/Player.cpp
@@ -0,0 +1,8 @@
+#include "Player.h"
+
+Player::Player() {
+	x = 0;
+	y = 0;
+	z = 0;
+	jump = 0;
+}
--- src/Renderer.h
+++ src/Renderer.h
@@ -3,6 +3,7 @@
 
 #include "Tunnel.h"
 #include "main.h"
+#include "Player.h"
 
 #include <SFML/Graphics.hpp>
 
@@ -10,12 +11,13 @@ class Renderer {
 	public:
 		Renderer();
 
-		void render(sf::RenderTarget& target, Tunnel& tunnel, float z);
+		void render(sf::RenderTarget& target, Tunnel& tunnel, Player& player);
 
 	private:
 		void drawTile(sf::VertexArray& vertexArray, float z, int x1, int x2, int y1, int y2, bool color);
 
-		sf::Texture texture;
+		sf::Texture tileTexture;
+		sf::Texture playerTexture;
 };
 
 #endif // RENDERER_H
